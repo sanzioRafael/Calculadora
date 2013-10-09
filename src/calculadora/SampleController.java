6d580dc4f7b3236a1fe0dda8844d9feb0385c300
Rafael Sanzio Macedo Porto <rafael.sanzio@outlook.com>
2013-10-09 07:40:54 -0400

    Commit inicial do projeto

--- src/calculadora/SampleController.java
+++ src/calculadora/SampleController.java
@@ -4,17 +4,14 @@
  */
 package calculadora;
 
-import java.awt.event.KeyEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javax.swing.JOptionPane;
 
 /**
  *
